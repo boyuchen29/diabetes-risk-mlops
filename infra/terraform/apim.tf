c32e7ff1661f45b6f555f604d7bf15e5ed4135e9
boyuchen29 <boyuchen29@outlook.com>
2026-04-23 21:56:38 -0700

    update apim property

--- infra/terraform/apim.tf
+++ infra/terraform/apim.tf
@@ -3,7 +3,7 @@ resource "azurerm_api_management" "apim" {
   location            = var.location
   resource_group_name = var.resource_group_name
   publisher_name      = "Diabetes Risk API"
-  publisher_email     = "xoxo.boyu@gmail.com"
+  publisher_email     = "boyu.chen@neudesic.com"
   sku_name            = "Consumption_0"
 }
 
